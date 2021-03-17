982af2b124074aa9be8c1783e5b0da5146a45840
gpsoft <etoh.gpsoft@gmail.com>
2021-03-18 06:30:55 +0900

    WIP: article crud - create

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -3,9 +3,16 @@ namespace App\Controller;
 
 class ArticlesController extends AppController
 {
+    public function initialize(): void
+    {
+        parent::initialize();
+
+        $this->loadComponent('Paginator');
+        $this->loadComponent('Flash');
+    }
+
     public function index()
     {
-        $this->loadComponent('Paginator');
         $articles = $this->Paginator->paginate($this->Articles->find());
         $this->set(compact('articles'));
     }
@@ -13,6 +20,23 @@ class ArticlesController extends AppController
     public function view($slug=null)
     {
         $article = $this->Articles->findBySlug($slug)->firstOrFail();
+        $this->set(compact('article'));
+    }
+
+    public function add()
+    {
+        $article = $this->Articles->newEmptyEntity();
+        if ( $this->request->is('post') ) {
+            $article = $this->Articles->patchEntity($article, $this->request->getData());
+            $article->user_id = 1;
+            // TODO: user_id from login user
+
+            if ( $this->Articles->save($article) ) {
+                $this->Flash->success('記事を保存しました。');
+                return $this->redirect(['action'=>'index']);
+            }
+            $this->Flash->error('記事の保存に失敗しました');
+        }
         $this->set(compact('article'));
     }
 }
--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -2,11 +2,23 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+use Cake\Utility\Text;
+use Cake\Event\EventInterface;
 
 class ArticlesTable extends Table
 {
     public function initialize(array $config) : void
     {
         $this->addBehavior('Timestamp');
+    }
+
+    public function beforeSave(EventInterface $event, $entity, $options)
+    {
+        if ( $entity->isNew() && !$entity->slug ) {
+            $sluggedTitle = Text::slug($entity->title);
+            $entity->slug = substr($sluggedTitle, 0, 191);
+            // TODO: slug should be unique
+            // TODO: make 191 constant?
+        }
     }
 }
